16fb47ffb5cab580f8d13c71872d084196aca62d
Jeyker <jeyker.salinas13@gmail.com>
2022-08-23 21:29:33 -0400

    Google analytics

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
 import "@/assets/tailwind.css";
 import "@/assets/main.scss";
 
 Vue.config.productionTip = false;
-
+Vue.use(VueGtag, {
+  config: { id: "328440093" }
+});
 new Vue({
   router,
   store,
